e2bcc4e82c63d73380bf8c3bac3f2b18298ef670
Kristin Kraemer <kristinhannahk@gmail.com>
2019-11-17 16:55:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ function writeCards(array, eventName) {
 }
 
 function countDown(num){
+<<<<<<< HEAD
   while (num > -1) {
     console.log(num);
     num--;
+=======
+  while(num > 0){
+    console.log(num);
+    let num = num -1;
+>>>>>>> 26658518aa61363b04c1d65dd8e9dc331add6992
   }
 }
